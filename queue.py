152f1c958fa6c71fdba2c9726346753a615389a1
mansi agarwal <agarwalmansi05@gmail.com>
2019-03-07 15:29:47 +0530

    Modified queue file

--- queue.py
+++ queue.py
@@ -4,14 +4,20 @@ class queue:
 		
 	# Element is inserted at the end	
 	def insert(self,val):
-		q.insert(len(self.q),val)
+		self.q.insert(len(self.q),val)
 	
 	# Remove element from front
 	def remove(self):
 		if len(self.q)==0:
 			return "Empty Queue!"
 		else:
-			return aelf.q.pop()
+			return self.q.pop(0)
+
+	def isempty(self):
+		if(len(self.q)==0):
+			return True
+		else:
+			return False
 			
 	# Print all the elements of the queue
 	def display(self):
